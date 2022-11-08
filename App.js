76d64b8ca6820134fcf97c2a5458b0480827e5e1
Jonathan-Evey <jonathan.c.evey@gmail.com>
2022-11-08 08:06:51 -0800

    add Expense Screen to nav stack

--- App.js
+++ App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LandingScreen from './screens/LandingScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
 import HomeScreen from './screens/HomeScreen';
+import ExpensesScreen from './screens/ExpensesScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen name="Landing" options={{headerShown: false}} component={LandingScreen} />
-        <Stack.Screen name="Login" options={{headerTitleAlign: 'center', headerLargeTitleShadowVisible: false, headerTransparent: true}} component={LoginScreen} />
-        <Stack.Screen name="Sign Up" options={{headerTitleAlign: 'center', headerLargeTitleShadowVisible: false, headerTransparent: true}} component={SignupScreen} />
-        <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+        <Stack.Screen
+          name="Landing"
+          options={{headerShown: false}}
+          component={LandingScreen}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{
+            headerTitleAlign: 'center',
+            headerLargeTitleShadowVisible: false,
+            headerTransparent: true,
+          }}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Sign Up"
+          options={{
+            headerTitleAlign: 'center',
+            headerLargeTitleShadowVisible: false,
+            headerTransparent: true,
+          }}
+          component={SignupScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          options={{headerShown: false}}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Expenses"
+          options={{
+            headerTitleAlign: 'center',
+            headerLargeTitleShadowVisible: false,
+            headerTransparent: true,
+          }}
+          component={ExpensesScreen}
+        />
       </Stack.Navigator>
-  </NavigationContainer>
+    </NavigationContainer>
   );
-}
+};
 
 export default App;
